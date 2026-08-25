21bc0e61ee0d7190519a19d0c1d2cb0e81b17f3d
Lucas <lucas.vale.marx@gmail.com>
2026-08-25 17:04:38 -0300

    feat: adição de saque na contaSalario

--- sistema.py
+++ sistema.py
@@ -5,12 +5,14 @@ class contaBancaria:
         self._id = id
 
         print(f'Bem vindo à sua conta: <{self._conta}>, id: {self._id}. Seu saldo é de R${self._saldo:.2f}.')
+        print('------')
 
     def depositar(self, deposito):
         self._deposito = deposito
         self._saldo += deposito
         print(f'Deposito de R${deposito:.2f} na conta <{self._conta}>.')
         print(f'Novo saldo: R${self._saldo:.2f}')
+        print('------')
 
     def sacar(self, saque):
         self.saque = saque
@@ -18,9 +20,11 @@ class contaBancaria:
             self._saldo -= saque
             print(f'Saque de R${saque:.2f} na conta <{self._conta}>.')
             print(f'Novo saldo da conta numero <{self._conta}> em nome de <{self._id}>: R${self._saldo:.2f}.')
+            print('------')
         else:
             print(f'Falha na tentativa de sacar R${saque:.2f}. Saldo em conta: R${self._saldo:.2f}.')
             print('nao tem dinheiro fdp vai toma no seu cu')
+            print('------')
 
     def depositar_com_taxa(self, deposito, taxa):
         self._taxa_deposito = deposito * (taxa * 0.01)
@@ -30,21 +34,37 @@ class contaBancaria:
         print(f'Você depositou R${deposito:.2f}. Foi cobrada taxa de {taxa}%. Total depositado: R${self._deposito_com_taxa:.2f}.')
         print(f'Taxa: {taxa}%. Valor da taxa: R${self._taxa_deposito:.2f}')
         print(f'Valor em saldo: R${self._saldo:.2f}.')
-
+        print('------')
     def saque_com_bonus(self, saque, bonus):
         self._bonus_saque = saque * (bonus * 0.01)
         self._saque_com_bonus = saque + self._bonus_saque
         if self._saldo >= saque:
             self._saldo -= self._saque_com_bonus
             print(f'Valor do saque: R${saque:.2f}. Valor do bonus de {bonus}% no saque: R${self._bonus_saque:.2f}.')
+            print('------')
         else:
             print(f'Saldo insuficiente para saque. Saldo: R${self._saldo:.2f}. vai toma no seu cu')
+            print('------')
+
+#conta com restrição de depósito, apenas saque
 
 class contaSalario(contaBancaria):
-    def __init__(self, saque):
-        self.saque = saque
+    def __init__(self, saldo, conta, id):
+        self._saldo = saldo
+        self._conta = conta
+        self._id = id
 
-
+    def sacar(self, saque):
+        if self._saldo >= saque:
+            self._saldo -= saque
+            print(f'Saque de R${saque:.2f} realizado.')
+            print(f'Saldo disponível na conta salário: R${self._saldo:.2f}.')
+        else:
+            print(f'Impossível sacar R${saque:.2f}. Valor em conta: R${self._saldo:.2f}.')
+        
+    def depositar(self):
+        raise NotImplementedError('Impossível depositar na conta salário, seu merda')
+    
 conta1 = contaBancaria(0, 33, 'lucas')
 conta1.depositar(500)
 conta1.sacar(230)
@@ -58,3 +78,6 @@ conta2.depositar_com_taxa(2113, 3)
 conta2.sacar(422)
 conta2.depositar_com_taxa(302, 3)
 conta2.saque_com_bonus(300, 10)
+
+salario1 = contaSalario(312, 2, 'luquinhas')
+salario1.sacar(311)
