cc4d534a26ed4ae51fedfa77939f60ac6b7506a0
Lucas <lucas.vale.marx@gmail.com>
2026-08-28 01:07:47 -0300

    feat: adição completa da função geradora de vendas conectada às outras tabelas

--- dados.py
+++ dados.py
@@ -1,17 +1,21 @@
 import sqlite3
-from gerador_dados import gera_dados_cliente, gera_dados_produtos, produtos_eletronicos
+from gerador_dados import gera_dados_cliente, gera_dados_produtos, gera_dados_vendas, produtos_eletronicos
 
 
-banco1 = gera_dados_cliente('pt_BR', 500)
-banco1.gera_nomes()
-banco1.gera_emails()
-banco1.gera_telefone()
+# banco1 = gera_dados_cliente('pt_BR', 500)
+# banco1.gera_nomes()
+# banco1.gera_emails()
+# banco1.gera_telefone()
 
-banco2 = gera_dados_produtos(produtos_eletronicos, 45)
-banco2.gera_produto()
+# dados_banco_1 = list(zip(banco1.lista_nomes, banco1.emails, banco1.lista_telefones))
 
-dados_banco_1 = list(zip(banco1.lista_nomes, banco1.emails, banco1.lista_telefones))
-dados_banco_2 = banco2.lista_produtos
+
+# banco2 = gera_dados_produtos(produtos_eletronicos, 45)
+# banco2.gera_produto()
+
+# dados_banco_2 = banco2.lista_produtos
+
+banco3 = gera_dados_vendas('banco_loja.db')
 
 
 conexao = sqlite3.connect('banco_loja.db')
@@ -34,20 +38,20 @@ cursor = conexao.cursor()
 #                 estoque_atual INTEGER NOT NULL)
 #                 """)
 
-cursor.execute(f"""
-                CREATE TABLE IF NOT EXISTS vendas
-                (id_venda INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                data_venda TEXT NOT NULL,
-                id_cliente INTEGER,
-                id_produto INTEGER,
-                quantidade INTEGER NOT NULL,
-                valor_total INTEGER NOT NULL,
-                FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-                FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
-                )
-                """)
+# cursor.execute(f"""
+#                 CREATE TABLE IF NOT EXISTS vendas
+#                 (id_venda INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+#                 data_venda TEXT NOT NULL,
+#                 id_cliente INTEGER,
+#                 id_produto INTEGER,
+#                 quantidade INTEGER NOT NULL,
+#                 valor_total INTEGER NOT NULL,
+#                 FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+#                 FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+#                 )
+#                 """)
 
-cursor.execute('SELECT id_clientes FROM clientes')
+# cursor.execute('DELETE FROM vendas WHERE ')
 
 
 
@@ -56,9 +60,15 @@ if __name__ == '__main__':
 #      'INSERT INTO clientes (nome, email, telefone) VALUES (?, ?, ?)',
 #      dados_banco_1)
 #   print(f'{len(dados_banco_1)} colunas adicionadas')
-    # cursor.executemany('INSERT INTO produtos (produto, preco_venda, preco_custo, estoque_atual) VALUES (?, ?, ?, ?)',
-    #                     dados_banco_2)
+    for _ in range(120):
+        banco3.buscar_ids()
+        banco3.gerar_datas()
+        banco3.distribuir_vendas()
+        cursor.executemany("""INSERT INTO vendas (data_venda, id_cliente, id_produto, quantidade, valor_total) VALUES (?, ?, ?, ?, ?)""",
+                        [(banco3.data_formatada, banco3.id_cliente, banco3.id_produto, banco3.quantidade_venda, banco3.valor_total)])
     # cursor.execute('DROP TABLE IF EXISTS vendas')
     # cursor.execute('ALTER TABLE clientes RENAME COLUMN id TO id_clientes')
+
+    # print(f'data venda: {banco3.data_formatada}, id do cliente: {banco3.id_cliente}, id do produto: {banco3.id_produto}, quantidade de vendas: {banco3.quantidade_venda}, valor total: {banco3.valor_total}')
     conexao.commit()
     conexao.close()
--- gerador_dados.py
+++ gerador_dados.py
@@ -1,6 +1,9 @@
 import random
+import sqlite3
 from faker import Faker
 from context_manager_e_log import log_execucao
+from datetime import datetime, timedelta
+import calendar
 
 produtos_eletronicos = {
     'smartphone': 1500,
@@ -17,7 +20,6 @@ produtos_eletronicos = {
 }
 
 # gerar 3 tabelas (clientes, produtos, vendas) p/ fazer consultas complexas
-
 
 
 class gera_dados_cliente:
@@ -75,8 +77,55 @@ class gera_dados_produtos:
 
         return self.lista_produtos
 
+class gera_dados_vendas:
+    def __init__(self, banco_dados):
+        self.banco_dados = banco_dados
+        self.connection = sqlite3.connect(banco_dados)
+        self.cursor = self.connection.cursor()
 
+    @log_execucao
+    def buscar_ids(self):
+        self.cursor.execute('SELECT id_clientes FROM clientes')
+        self.clientes_ids = [row[0] for row in self.cursor.fetchall()]
+
+        self.cursor.execute('SELECT id_produto, preco_venda FROM produtos')
+        self.produtos = self.cursor.fetchall()
+        self.produtos_ids = [row[0] for row in self.produtos]
+        self.precos_produtos = {row[0]: row[1] for row in self.produtos}
+
+    @log_execucao
+    def gerar_datas(self):
+        self.dt = datetime.now()
+        self.data_fim = self.dt.date()
+        self.data_inicio = self.data_fim - timedelta(days=180)
+
+        self.dias_entre = (self.data_fim - self.data_inicio).days
+        self.data_dias_entre = random.randint(0, self.dias_entre)
+        self.data_venda = self.data_inicio + timedelta(days=self.data_dias_entre)
+        self.data_formatada = self.data_venda.strftime('%d/%m/%Y')
+        return self.data_formatada
+    
+    @log_execucao    
+    def distribuir_vendas(self):
+        self.id_cliente = random.choice(self.clientes_ids)
+        self.id_produto = random.choice(self.produtos_ids)
+        self.preco_venda = self.precos_produtos[self.id_produto]
+
+        self.quantidade_venda = random.choices([1, 2, 3, 4], weights = [30, 20, 10, 5])[0]
+        self.valor_total = self.preco_venda * self.quantidade_venda
+
+        return self.id_cliente, self.id_produto, self.quantidade_venda, self.valor_total
+    
 if __name__ == '__main__':
-    produtos1 = gera_dados_produtos(produtos_eletronicos, 45)
-    produtos1.gera_produto()
-    print(produtos1.lista_produtos, len(produtos1.lista_produtos))
+    # produtos1 = gera_dados_produtos(produtos_eletronicos, 45)
+    # produtos1.gera_produto()
+    # print(produtos1.lista_produtos, len(produtos1.lista_produtos))
+    conexao = sqlite3.connect('sqlite_db.db')
+    cursor = conexao.cursor()
+
+    produtos = gera_dados_vendas('sqlite_db.db')
+    produtos.buscar_ids()
+    produtos.gerar_datas()
+    produtos.distribuir_vendas()
+
+    print(produtos.quantidade_venda)
