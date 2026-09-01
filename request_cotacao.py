475967e927170c4fae831f7f64f9aa0a05ae0d5d
Lucas <lucas.vale.marx@gmail.com>
2026-09-01 16:26:32 -0300

    feat: teste de requests com awesomeapi

--- request_cotacao.py
+++ request_cotacao.py
@@ -5,12 +5,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/get-data')
-def pegar_cotacao():
+def testar_request():
     resposta = requests.get('https://economia.awesomeapi.com.br/')
     if resposta.status_code == 200:
         return resposta.json()
     else:
         return {'erro': f'status: {resposta.status_code}'}
 
-teste = pegar_cotacao()
+teste = testar_request()
 print(teste)
+
+@app.get('get-data')
+def testar_moeda(moeda):
+    resposta = requests.get(f'https://economia.awesomeapi.com.br/json/last/:moedas, {moeda}')
+    return resposta.json()
