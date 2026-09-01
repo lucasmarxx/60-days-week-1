c16254678755d24156a7241a7ba898b20b8ca691
Lucas <lucas.vale.marx@gmail.com>
2026-09-01 16:43:31 -0300

    cotacoes

--- request_cotacao.py
+++ request_cotacao.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
+from minha_key_api_nao_upar import chave
 
 app = FastAPI()
+chave_api = chave
 
 @app.get('/get-data')
 def testar_request():
@@ -13,9 +15,13 @@ def testar_request():
         return {'erro': f'status: {resposta.status_code}'}
 
 teste = testar_request()
-print(teste)
+# print(teste)
 
 @app.get('get-data')
-def testar_moeda(moeda):
-    resposta = requests.get(f'https://economia.awesomeapi.com.br/json/last/:moedas, {moeda}')
+def testar_moeda(moedas):
+    resposta = requests.get(f'https://economia.awesomeapi.com.br/json/last/{moedas}')
     return resposta.json()
+
+testar_moeda('USD-BRL')
+moeda = testar_moeda('USD-BRL')
+print(moeda)
