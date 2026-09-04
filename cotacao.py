874dfb8dae1fbc47ff150a377a027ccb7d277fe1
Lucas <lucas.vale.marx@gmail.com>
2026-09-04 12:27:52 -0300

    feat: criação de tabela de cotações, falta adicionar os dados que serão capturados na api

--- cotacao.py
+++ cotacao.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI
 import pprint
 from minha_key import chave
+import sqlite3
+from datetime import datetime
+
 
 app = FastAPI()
 chave_api = chave
@@ -28,4 +31,20 @@ cotacao_euro = pegar_cotacao('EUR-BRL')
 
 valor_atual_dolar = cotacao_dolar['USDBRL']['bid']
 valor_atual_euro = cotacao_euro['EURBRL']['bid']
-print(valor_atual_dolar, valor_atual_euro)
+
+conexao = sqlite3.connect('banco_cotacoes.db')
+cursor = conexao.cursor()
+
+# cursor.execute(f"""
+#                 CREATE TABLE IF NOT EXISTS cotacoes
+#                 (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+#                 nome TEXT NOT NULL,
+#                 preco REAL,
+#                 variacao REAL, 
+#                 data_hora DATETIME DEFAULT CURRENT_TIMESTAMP
+#                 )
+#                 """)
+cursor.execute(f"""
+                ALTER TABLE cotacoes ADD COLUMN moeda TEXT
+""")
+conexao.commit()
