b0c3454c4d7ca518ccf70ff7712b7210cfcb1c3f
Lucas <lucas.vale.marx@gmail.com>
2026-09-04 12:06:12 -0300

    feat: testes com pandas

--- dados_pandas.py
+++ dados_pandas.py
@@ -10,8 +10,16 @@ df = pd.read_csv('01_EWC2026_Event_Tournament_Summary.csv')
 
 # print(df.info())
 # print(df.describe())
-print(df.head(2))
+# print(df.head(2))
 filtro_vice = df['Runner_Up']
 vencedor_e_premio = df[['Prize_Pool_USD', 'Winner']]
 
-print(vencedor_e_premio)
+filtro_1 = df[df['Gender'] == "Women's"]
+filtro_2 = df[df['Prize_Pool_USD'] > 500000]
+filtro_3 = df[df['Platform'] == 'Mobile']
+filtro_4 = df[df['Game_Type'] == 'MOBA']
+
+print(f'{filtro_1}\n{"-" * 200}')
+print(f'{filtro_2}\n{"-" * 200}')
+print(f'{filtro_3}\n{"-" * 200}')
+print(f'{filtro_4}\n{"-" * 200}')
